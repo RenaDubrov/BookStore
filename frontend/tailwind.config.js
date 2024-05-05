27904ed401b29192e648ca620267a2f7a614b907
Rena Dubrov <miss.dubrov@gmail.com>
2024-05-04 20:23:57 -0400

    small comment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    // external content will require CORS
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -9,4 +10,3 @@ export default {
   },
   plugins: [],
 }
-
